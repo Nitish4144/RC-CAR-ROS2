e29021c15a0211903b8a7722feeb2710c4000dd1
Nitish <devadanitish@gmail.com>
2025-09-27 22:57:30 +0530

    Update gym_bridge.py

--- src/simulator/simulator/gym_bridge.py
+++ src/simulator/simulator/gym_bridge.py
@@ -1,47 +1,26 @@
 # MIT License
 
-# Copyright (c) 2024 S7our Squad
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
+# ROS2 and Simulation related imports
 import rclpy
 from rclpy.node import Node
-
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import TransformStamped
-from geometry_msgs.msg import Transform
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Twist, TransformStamped, Transform
 from ackermann_msgs.msg import AckermannDriveStamped
 from tf2_ros import TransformBroadcaster
-
 import gym
 import numpy as np
 from transforms3d import euler
 
 class GymBridge(Node):
+    """
+    ROS2 Node acting as a bridge between the F110 Gym simulation environment
+    and ROS2 topics. Supports single or dual (opponent) cars.
+    """
     def __init__(self):
         super().__init__('gym_bridge')
 
+        # Declare and fetch parameters from ROS2 parameter server
         self.declare_parameter('ego_namespace')
         self.declare_parameter('ego_odom_topic')
         self.declare_parameter('ego_opp_odom_topic')
@@ -66,27 +45,28 @@ class GymBridge(Node):
         self.declare_parameter('stheta1')
         self.declare_parameter('kb_teleop')
 
-        # check num_agents
+        # Handle multi-agent (ego/opponent) setup
         num_agents = self.get_parameter('num_agent').value
-        if num_agents < 1 or num_agents > 2:
-            raise ValueError('num_agents should be either 1 or 2.')
-        elif not isinstance(num_agents, int):
-            raise ValueError('num_agents should be an int.')
+        if num_agents not in [1, 2] or not isinstance(num_agents, int):
+            raise ValueError('num_agents should be either 1 or 2 and int.')
 
-        # env backend
+        # Setup F1Tenth Gym environment
         self.env = gym.make('f110_gym:f110-v0',
                             map=self.get_parameter('map_path').value,
                             map_ext=self.get_parameter('map_img_ext').value,
                             num_agents=num_agents)
 
+        # Initial positions and pose state
         sx = self.get_parameter('sx').value
         sy = self.get_parameter('sy').value
         stheta = self.get_parameter('stheta').value
-        self.ego_pose = [sx, sy, stheta]
-        self.ego_speed = [0.0, 0.0, 0.0]
-        self.ego_requested_speed = 0.0
-        self.ego_steer = 0.0
-        self.ego_collision = False
+        self.ego_pose = [sx, sy, stheta]                # Ego car pose (x, y, theta)
+        self.ego_speed = [0.0, 0.0, 0.0]                # Ego car speed (vx, vy, wz)
+        self.ego_requested_speed = 0.0                  # Commanded speed (from ROS)
+        self.ego_steer = 0.0                            # Commanded steering
+        self.ego_collision = False                      # Flag for collision event
+
+        # Topics and scan configuration
         ego_scan_topic = self.get_parameter('ego_scan_topic').value
         ego_drive_topic = self.get_parameter('ego_drive_topic').value
         scan_fov = self.get_parameter('scan_fov').value
@@ -97,7 +77,8 @@ class GymBridge(Node):
         self.ego_namespace = self.get_parameter('ego_namespace').value
         ego_odom_topic = self.ego_namespace + '/' + self.get_parameter('ego_odom_topic').value
         self.scan_distance_to_base_link = self.get_parameter('scan_distance_to_base_link').value
-        
+
+        # Opponent (second car) setup if present
         if num_agents == 2:
             self.has_opp = True
             self.opp_namespace = self.get_parameter('opp_namespace').value
@@ -109,129 +90,118 @@ class GymBridge(Node):
             self.opp_requested_speed = 0.0
             self.opp_steer = 0.0
             self.opp_collision = False
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[sx, sy, stheta], [sx1, sy1, stheta1]]))
+            # Reset environment with both agents' poses
+            self.obs, _, self.done, _ = self.env.reset(np.array([[sx, sy, stheta], [sx1, sy1, stheta1]]))
             self.ego_scan = list(self.obs['scans'][0])
             self.opp_scan = list(self.obs['scans'][1])
-
             opp_scan_topic = self.get_parameter('opp_scan_topic').value
             opp_odom_topic = self.opp_namespace + '/' + self.get_parameter('opp_odom_topic').value
             opp_drive_topic = self.get_parameter('opp_drive_topic').value
-
             ego_opp_odom_topic = self.ego_namespace + '/' + self.get_parameter('ego_opp_odom_topic').value
             opp_ego_odom_topic = self.opp_namespace + '/' + self.get_parameter('opp_ego_odom_topic').value
         else:
             self.has_opp = False
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[sx, sy, stheta]]))
+            # Reset environment with only ego agent
+            self.obs, _, self.done, _ = self.env.reset(np.array([[sx, sy, stheta]]))
             self.ego_scan = list(self.obs['scans'][0])
 
-        # sim physical step timer
+        # Timers for stepping sim and data publishing
         self.drive_timer = self.create_timer(0.01, self.drive_timer_callback)
-        # topic publishing timer
         self.timer = self.create_timer(0.004, self.timer_callback)
 
-        # transform broadcaster
+        # TF transform broadcaster
         self.br = TransformBroadcaster(self)
 
-        # publishers
+        # Publishers for ego and (optionally) opponent topics
         self.ego_scan_pub = self.create_publisher(LaserScan, ego_scan_topic, 10)
         self.ego_odom_pub = self.create_publisher(Odometry, ego_odom_topic, 10)
         self.ego_drive_published = False
-        if num_agents == 2:
+
+        if self.has_opp:
             self.opp_scan_pub = self.create_publisher(LaserScan, opp_scan_topic, 10)
             self.ego_opp_odom_pub = self.create_publisher(Odometry, ego_opp_odom_topic, 10)
             self.opp_odom_pub = self.create_publisher(Odometry, opp_odom_topic, 10)
             self.opp_ego_odom_pub = self.create_publisher(Odometry, opp_ego_odom_topic, 10)
             self.opp_drive_published = False
 
-        # subscribers
+        # Subscribers for drive commands, resets, and teleop (keyboard control)
         self.ego_drive_sub = self.create_subscription(
-            AckermannDriveStamped,
-            ego_drive_topic,
-            self.drive_callback,
-            10)
+            AckermannDriveStamped, ego_drive_topic, self.drive_callback, 10)
         self.ego_reset_sub = self.create_subscription(
-            PoseWithCovarianceStamped,
-            '/initialpose',
-            self.ego_reset_callback,
-            10)
-        if num_agents == 2:
+            PoseWithCovarianceStamped, '/initialpose', self.ego_reset_callback, 10)
+        if self.has_opp:
             self.opp_drive_sub = self.create_subscription(
-                AckermannDriveStamped,
-                opp_drive_topic,
-                self.opp_drive_callback,
-                10)
+                AckermannDriveStamped, opp_drive_topic, self.opp_drive_callback, 10)
             self.opp_reset_sub = self.create_subscription(
-                PoseStamped,
-                '/goal_pose',
-                self.opp_reset_callback,
-                10)
-
+                PoseStamped, '/goal_pose', self.opp_reset_callback, 10)
         if self.get_parameter('kb_teleop').value:
+            # Keyboard teleop
             self.teleop_sub = self.create_subscription(
-                Twist,
-                '/cmd_vel',
-                self.teleop_callback,
-                10)
+                Twist, '/cmd_vel', self.teleop_callback, 10)
 
+    # -- ROS Callbacks --
 
     def drive_callback(self, drive_msg):
+        """Handle drive commands for the ego car."""
         self.ego_requested_speed = drive_msg.drive.speed
         self.ego_steer = drive_msg.drive.steering_angle
         self.ego_drive_published = True
 
     def opp_drive_callback(self, drive_msg):
+        """Handle drive commands for opponent car (if present)."""
         self.opp_requested_speed = drive_msg.drive.speed
         self.opp_steer = drive_msg.drive.steering_angle
         self.opp_drive_published = True
 
     def ego_reset_callback(self, pose_msg):
+        """Reset ego car pose in simulation based on message."""
         rx = pose_msg.pose.pose.position.x
         ry = pose_msg.pose.pose.position.y
-        rqx = pose_msg.pose.pose.orientation.x
-        rqy = pose_msg.pose.pose.orientation.y
-        rqz = pose_msg.pose.pose.orientation.z
-        rqw = pose_msg.pose.pose.orientation.w
-        _, _, rtheta = euler.quat2euler([rqw, rqx, rqy, rqz], axes='sxyz')
+        orientation = pose_msg.pose.pose.orientation
+        _, _, rtheta = euler.quat2euler(
+            [orientation.w, orientation.x, orientation.y, orientation.z], axes='sxyz')
+        # If opponent present, keep its pose, reset ego only
         if self.has_opp:
             opp_pose = [self.obs['poses_x'][1], self.obs['poses_y'][1], self.obs['poses_theta'][1]]
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[rx, ry, rtheta], opp_pose]))
+            self.obs, _, self.done, _ = self.env.reset(np.array([[rx, ry, rtheta], opp_pose]))
         else:
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[rx, ry, rtheta]]))
+            self.obs, _, self.done, _ = self.env.reset(np.array([[rx, ry, rtheta]]))
 
     def opp_reset_callback(self, pose_msg):
+        """Reset opponent car pose in simulation (if 2 agents)."""
         if self.has_opp:
             rx = pose_msg.pose.position.x
             ry = pose_msg.pose.position.y
-            rqx = pose_msg.pose.orientation.x
-            rqy = pose_msg.pose.orientation.y
-            rqz = pose_msg.pose.orientation.z
-            rqw = pose_msg.pose.orientation.w
-            _, _, rtheta = euler.quat2euler([rqw, rqx, rqy, rqz], axes='sxyz')
-            self.obs, _ , self.done, _ = self.env.reset(np.array([list(self.ego_pose), [rx, ry, rtheta]]))
+            orientation = pose_msg.pose.orientation
+            _, _, rtheta = euler.quat2euler(
+                [orientation.w, orientation.x, orientation.y, orientation.z], axes='sxyz')
+            self.obs, _, self.done, _ = self.env.reset(np.array([list(self.ego_pose), [rx, ry, rtheta]]))
+
     def teleop_callback(self, twist_msg):
+        """Allow controlling ego car using keyboard commands (teleop)."""
         if not self.ego_drive_published:
             self.ego_drive_published = True
-
         self.ego_requested_speed = twist_msg.linear.x
+        # Set steering based on angular direction
+        self.ego_steer = 0.3 if twist_msg.angular.z > 0.0 else -0.3 if twist_msg.angular.z < 0.0 else 0.0
 
-        if twist_msg.angular.z > 0.0:
-            self.ego_steer = 0.3
-        elif twist_msg.angular.z < 0.0:
-            self.ego_steer = -0.3
-        else:
-            self.ego_steer = 0.0
+    # -- Timers and Publishing --
 
     def drive_timer_callback(self):
+        """Step simulation forward using recent drive commands."""
         if self.ego_drive_published and not self.has_opp:
             self.obs, _, self.done, _ = self.env.step(np.array([[self.ego_steer, self.ego_requested_speed]]))
         elif self.ego_drive_published and self.has_opp and self.opp_drive_published:
-            self.obs, _, self.done, _ = self.env.step(np.array([[self.ego_steer, self.ego_requested_speed], [self.opp_steer, self.opp_requested_speed]]))
+            self.obs, _, self.done, _ = self.env.step(np.array([
+                [self.ego_steer, self.ego_requested_speed],
+                [self.opp_steer, self.opp_requested_speed]
+            ]))
         self._update_sim_state()
 
     def timer_callback(self):
+        """Publish sensor data and TF transforms periodically."""
         ts = self.get_clock().now().to_msg()
-
-        # pub scans
+        # Publish ego scan
         scan = LaserScan()
         scan.header.stamp = ts
         scan.header.frame_id = self.ego_namespace + '/laser'
@@ -242,7 +212,7 @@ class GymBridge(Node):
         scan.range_max = 30.
         scan.ranges = self.ego_scan
         self.ego_scan_pub.publish(scan)
-
+        # Publish opponent scan if present
         if self.has_opp:
             opp_scan = LaserScan()
             opp_scan.header.stamp = ts
@@ -254,15 +224,16 @@ class GymBridge(Node):
             opp_scan.range_max = 30.
             opp_scan.ranges = self.opp_scan
             self.opp_scan_pub.publish(opp_scan)
-
-        # pub tf
+        # Publish odometry and transforms
         self._publish_odom(ts)
         self._publish_transforms(ts)
         self._publish_laser_transforms(ts)
         self._publish_wheel_transforms(ts)
 
     def _update_sim_state(self):
+        """Update stored states from the simulator outputs after a step."""
         self.ego_scan = list(self.obs['scans'][0])
+        # Update opponent if present
         if self.has_opp:
             self.opp_scan = list(self.obs['scans'][1])
             self.opp_pose[0] = self.obs['poses_x'][1]
@@ -271,7 +242,7 @@ class GymBridge(Node):
             self.opp_speed[0] = self.obs['linear_vels_x'][1]
             self.opp_speed[1] = self.obs['linear_vels_y'][1]
             self.opp_speed[2] = self.obs['ang_vels_z'][1]
-
+        # Ego state
         self.ego_pose[0] = self.obs['poses_x'][0]
         self.ego_pose[1] = self.obs['poses_y'][0]
         self.ego_pose[2] = self.obs['poses_theta'][0]
@@ -279,9 +250,9 @@ class GymBridge(Node):
         self.ego_speed[1] = self.obs['linear_vels_y'][0]
         self.ego_speed[2] = self.obs['ang_vels_z'][0]
 
-        
-
     def _publish_odom(self, ts):
+        """Publish Odometry messages for ego and opponent cars."""
+        # Ego odom
         ego_odom = Odometry()
         ego_odom.header.stamp = ts
         ego_odom.header.frame_id = 'map'
@@ -297,7 +268,7 @@ class GymBridge(Node):
         ego_odom.twist.twist.linear.y = self.ego_speed[1]
         ego_odom.twist.twist.angular.z = self.ego_speed[2]
         self.ego_odom_pub.publish(ego_odom)
-
+        # Publish opponent odom and cross-odom (for applications needing both car data)
         if self.has_opp:
             opp_odom = Odometry()
             opp_odom.header.stamp = ts
@@ -318,6 +289,8 @@ class GymBridge(Node):
             self.ego_opp_odom_pub.publish(opp_odom)
 
     def _publish_transforms(self, ts):
+        """Publish base_link TF transforms for both vehicles."""
+        # Ego transform
         ego_t = Transform()
         ego_t.translation.x = self.ego_pose[0]
         ego_t.translation.y = self.ego_pose[1]
@@ -327,14 +300,13 @@ class GymBridge(Node):
         ego_t.rotation.y = ego_quat[2]
         ego_t.rotation.z = ego_quat[3]
         ego_t.rotation.w = ego_quat[0]
-
         ego_ts = TransformStamped()
         ego_ts.transform = ego_t
         ego_ts.header.stamp = ts
         ego_ts.header.frame_id = 'map'
         ego_ts.child_frame_id = self.ego_namespace + '/base_link'
         self.br.sendTransform(ego_ts)
-
+        # Opponent transform if present
         if self.has_opp:
             opp_t = Transform()
             opp_t.translation.x = self.opp_pose[0]
@@ -345,7 +317,6 @@ class GymBridge(Node):
             opp_t.rotation.y = opp_quat[2]
             opp_t.rotation.z = opp_quat[3]
             opp_t.rotation.w = opp_quat[0]
-
             opp_ts = TransformStamped()
             opp_ts.transform = opp_t
             opp_ts.header.stamp = ts
@@ -354,6 +325,8 @@ class GymBridge(Node):
             self.br.sendTransform(opp_ts)
 
     def _publish_wheel_transforms(self, ts):
+        """Publish TF for front wheels steering (for both cars)."""
+        # Ego left and right front wheel hinges
         ego_wheel_ts = TransformStamped()
         ego_wheel_quat = euler.euler2quat(0., 0., self.ego_steer, axes='sxyz')
         ego_wheel_ts.transform.rotation.x = ego_wheel_quat[1]
@@ -364,10 +337,12 @@ class GymBridge(Node):
         ego_wheel_ts.header.frame_id = self.ego_namespace + '/front_left_hinge'
         ego_wheel_ts.child_frame_id = self.ego_namespace + '/front_left_wheel'
         self.br.sendTransform(ego_wheel_ts)
+
         ego_wheel_ts.header.frame_id = self.ego_namespace + '/front_right_hinge'
         ego_wheel_ts.child_frame_id = self.ego_namespace + '/front_right_wheel'
         self.br.sendTransform(ego_wheel_ts)
 
+        # Opponent wheels if present
         if self.has_opp:
             opp_wheel_ts = TransformStamped()
             opp_wheel_quat = euler.euler2quat(0., 0., self.opp_steer, axes='sxyz')
@@ -379,20 +354,21 @@ class GymBridge(Node):
             opp_wheel_ts.header.frame_id = self.opp_namespace + '/front_left_hinge'
             opp_wheel_ts.child_frame_id = self.opp_namespace + '/front_left_wheel'
             self.br.sendTransform(opp_wheel_ts)
+
             opp_wheel_ts.header.frame_id = self.opp_namespace + '/front_right_hinge'
             opp_wheel_ts.child_frame_id = self.opp_namespace + '/front_right_wheel'
             self.br.sendTransform(opp_wheel_ts)
 
     def _publish_laser_transforms(self, ts):
+        """Publish TF transform for LIDAR scan frame (for both cars)."""
         ego_scan_ts = TransformStamped()
         ego_scan_ts.transform.translation.x = self.scan_distance_to_base_link
-        # ego_scan_ts.transform.translation.z = 0.04+0.1+0.025
         ego_scan_ts.transform.rotation.w = 1.
         ego_scan_ts.header.stamp = ts
         ego_scan_ts.header.frame_id = self.ego_namespace + '/base_link'
         ego_scan_ts.child_frame_id = self.ego_namespace + '/laser'
         self.br.sendTransform(ego_scan_ts)
-
+        # Opponent LIDAR frame
         if self.has_opp:
             opp_scan_ts = TransformStamped()
             opp_scan_ts.transform.translation.x = self.scan_distance_to_base_link
@@ -403,6 +379,7 @@ class GymBridge(Node):
             self.br.sendTransform(opp_scan_ts)
 
 def main(args=None):
+    """Entry point for running the ROS2 gym bridge node."""
     rclpy.init(args=args)
     gym_bridge = GymBridge()
     rclpy.spin(gym_bridge)
