ab50d1c57e4fe9d57b54a2a6c6283e4d818ae5ff
kmanojkr12-bit <k.manoj.k.r12@gmail.com>
2025-10-26 15:19:45 +0530

    Add setup.py for lcd_eyes package
    Set up packaging configuration for lcd_eyes module.

--- src/lcd_eyes/setup.py
+++ src/lcd_eyes/setup.py
@@ -1,1 +1,25 @@
+from setuptools import setup
+
+package_name = 'lcd_eyes'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'Pillow', 'adafruit-circuitpython-ssd1306'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='LCD eyes display node for RC car (ROS2)',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'eye_display = lcd_eyes.eye_display:main',
+        ],
+    },
+)
 
