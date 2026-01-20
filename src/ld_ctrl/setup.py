281d660f8af2dd960a2ed54698e63274762a17f2
na24b064 <na24b064@smail.iitm.ac.in>
2026-01-20 22:40:19 +0530

    Create setup.py

---
+++ src/ld_ctrl/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ld_ctrl'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/ld_ctrl.launch.py']),
+        ('share/' + package_name + '/config', ['config/ydlidar.yaml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='YOUR_NAME',
+    maintainer_email='YOUR_EMAIL@example.com',
+    description='LiDAR control and configuration package for RC-CAR-ROS2',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
